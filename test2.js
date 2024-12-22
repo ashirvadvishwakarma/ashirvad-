b0ac2895a12b12464f924fa5f11128f9ec168fe8
ashirvadvishwakarma <kashirvad12@gmail.com>
2024-12-22 20:59:06 +0000

    container config done

---
+++ test2.js
@@ -0,0 +1,14 @@
+const password= 1234
+let accountemail="jbvsb@gmail.com"
+accountcity="jaipur"
+let accountstate;
+
+
+
+
+
+accountemail="kaashi@gamil.com"
+accountpassword="jhdsbfh"
+accountcity="delhi"
+
+console.log([accountemail,accountpassword,accountcity,accountstate]);
